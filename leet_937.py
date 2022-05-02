76222fdbea461f5c4a4689bdc4c7bcc589cd5442
haeunY153 <silanm115@gmail.com>
2022-05-02 23:12:19 +0900

    Reorder log files

--- leet_937.py
+++ leet_937.py
@@ -6,7 +6,7 @@
 
 logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
 
-# 식별자 구분을 위한 리스트 만들기
+식별자 구분을 위한 리스트 만들기
 def reorder_log(logs):
     let = []
     dig = []
@@ -22,3 +22,24 @@ def reorder_log(logs):
 
 print(reorder_log(logs)) 
 
+########################################################################################
+
+from typing import List # 원 문제 형식 그대로 가져올 때 타이핑 모듈_인자 자료형 불러오기
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]: # logs: List[str]) -> List[str] 주석 무시
+    
+        let = []
+        dig = []
+
+        for log in logs:
+            if log.split()[1].isdigit(): # Return T/F
+                dig.append(log)
+            else:
+                let.append(log)
+
+        let.sort(key = lambda x: (x.split()[1:], x.split()[0]))
+        return let + dig
+
+logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+Sol = Solution() # 함수 호출 시 인스턴스 객체 꼭 해주기!!
+print(Sol.reorderLogFiles(logs))
